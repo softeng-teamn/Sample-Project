2624a1c6863fca1e791d7adb330820049cd80221
samgoldman <sgoldman216@gmail.com>
2019-03-19 21:51:49 -0400

    Add stubs for other DBController tests

---
+++ src/test/java/sample/DBControllerTest.java
@@ -0,0 +1,38 @@
+package sample;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DBControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void init() {
+    }
+
+    @Test
+    public void init1() {
+    }
+
+    @Test
+    public void insertHistory() {
+    }
+
+    @Test
+    public void getRecentHistory() {
+    }
+
+    @Test
+    public void getRecentHistory1() {
+    }
+}
