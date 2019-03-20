7d2f15e343e59f2238d5add453ee2e6f218f32f7
Tyler Ferrara <tylerferrara123@gmail.com>
2019-03-20 11:48:14 -0400

    ui created and connected

--- src/main/java/control/Controller.java
+++ src/main/java/control/Controller.java
@@ -1,4 +1,27 @@
 package control;
 
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class Controller {
+
+    @FXML
+    Label inputDisplay;
+
+    public void Controller() {}
+
+    @FXML
+    private void initialize() {
+    }
+
+
+    @FXML
+    private void btnPressed(ActionEvent event) {
+        System.out.println("hi there!!!");
+        Button btn = (Button) event.getSource();
+        System.out.println(btn.getText());
+    }
+
 }
